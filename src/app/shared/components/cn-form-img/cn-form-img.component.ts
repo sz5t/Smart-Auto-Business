ec181788141b2d063ca1626b73017e5b7b4113d3
18700085881 <1224812884@qq.com>
2019-03-15 15:04:02 +0800

    表单图片

--- src/app/shared/components/cn-form-img/cn-form-img.component.ts
+++ src/app/shared/components/cn-form-img/cn-form-img.component.ts
@@ -18,8 +18,10 @@ export class CnFormImgComponent implements OnInit {
   @Input() public initValue;
   @Input() private changeConfig;
   @Output() public updateValue = new EventEmitter();
+
+  public _value;
   public bodyStyle = {
-    width: '100 %' ,
+    width: '100 %',
     height: '50px'
 
   };
@@ -27,17 +29,16 @@ export class CnFormImgComponent implements OnInit {
 
   public ngOnInit() {
     if (!this.value) {
-      if (this.formGroup.value[this.config.name]) {
-        this.value = this.formGroup.value[this.config.name];
-      } else {
-        if (this.config.hasOwnProperty('defaultValue')) {
-          this.value = this.config.defaultValue;
-        }
+      if (this.config.hasOwnProperty('defaultValue')) {
+        this.value = this.config.defaultValue;
       }
     }
-   //  this.value = 'http://192.168.1.111:8081/api.cfg/files/upload/2019-03-12/44bb4a6551dd4f3984d49c470ed5c07a.jpg'
-    // console.log(this.value);
+    //  this.value = 'http://192.168.1.111:8081/api.cfg/files/upload/2019-03-12/44bb4a6551dd4f3984d49c470ed5c07a.jpg'
+    // console.log(this.value, this._value);
   }
-
+  public valueChange(name?) {
+    this.value.data = name;
+    this.updateValue.emit(this.value);
+  }
 
 }
