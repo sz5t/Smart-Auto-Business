cac4390c0bc611884f52d2c85c2e2e9ad7b43e04
zxlgrandia <grandia-4727@163.com>
2019-05-24 10:28:02 +0800

    Update webConfig.js

--- src/assets/webConfig.js
+++ src/assets/webConfig.js
@@ -1,14 +1,54 @@
 module.exports = {
-    /** web服务  url: "http://192.168.1.111:8083"  http://localhost:4200*/
-     localResourceUrl: "http://localhost:4200",
-     /** 后台服务 */
-    settingSystemServer: 'http://192.168.1.111:12345/api.cfg/',
-    // settingSystemServer: 'http://localhost:8081/api.cfg/',
+  //-------------开发配置
+  // localResourceUrl: "http://localhost:4200",
+  //  /** 后台服务 */
+  // settingSystemServer: 'http://192.168.1.111:8888/api.cfg/',
+  // /** 后台服务 */
+  // appSystemServer: 'http://192.168.1.111:8888/api.cfg/',
+  // /**报表  */
+  // reportServerUrl: 'http://192.168.1.111:8088/ReportServer.ashx'
+
+
+  // 公司服务
+   localResourceUrl: "http://localhost:4200",
     /** 后台服务 */
-     appSystemServer: 'http://192.168.1.111:12345/api.cfg/',
-    //appSystemServer: 'http://localhost:8081/api.cfg/',
-    /**报表  */
-    reportServerUrl: 'http://192.168.1.111:12345/ReportServer.ashx'
-    //reportServerUrl: 'http://localhost:8088/ReportServer.ashx'
+   settingSystemServer: 'http://192.168.1.111:8081/api.cfg/',
+   /** 后台服务 */
+   appSystemServer: 'http://192.168.1.111:8081/api.cfg/',
+   /**报表  */
+   reportServerUrl: 'http://192.168.1.111:8088/ReportServer.ashx'
+
+
+  // 三发服务
+
+  // localResourceUrl: "http://192.168.1.111:8083",
+  // /** 后台服务 */
+  // settingSystemServer: 'http://192.168.1.111:8888/api.cfg/',
+  // /** 后台服务 */
+  // appSystemServer: 'http://192.168.1.111:8888/api.cfg/',
+  // /**报表  */
+  // reportServerUrl: 'http://192.168.1.111:8088/ReportServer.ashx'
+
+  //-------------单机配置
+  // localResourceUrl: "http://localhost:8083",
+  // /** 后台服务 */
+  // settingSystemServer: 'http://localhost:8081/api.cfg/',
+  // /** 后台服务 */
+  // appSystemServer: 'http://localhost:8081/api.cfg/',
+  // /**报表  */
+  // reportServerUrl: 'http://localhost:8088/ReportServer.ashx'
+
+
+
+  //-------------四院服务器
+  // localResourceUrl: "http://10.129.7.4:8083",
+  // /** 后台服务 */
+  // settingSystemServer: 'http://10.129.7.4:8081/api.cfg/',
+  // /** 后台服务 */
+  // appSystemServer: 'http://10.129.7.4:8081/api.cfg/',
+  // /**报表  */
+  // reportServerUrl: 'http://10.129.7.4:8088/ReportServer.ashx'
+
+
 }
 
